24174b0a246ebdb8a5d0c0fc37cb8b1a295cbea8
justno1 <justno1@hotmail.com.tw>
2014-06-10 23:51:56 +0800

    try

--- js/script.js
+++ js/script.js
@@ -87,8 +87,8 @@ FB.getLoginStatus(function(response) {
 	var ctx = document.getElementById('canvas').getContext('2d'); //宣告變數找到頁面的canvas標籤的2d內容
 	ctx.font='20px "Arial"'; //設定字體與大小
 	ctx.fillText("Click here to start fill with Facebook Profile Picture", 40, 270); //設定預設的開始畫面
-    var img = new Image(); // 新增圖像1
-    img.src = "img/overlay.png"; //圖像路徑（路徑自己設，且自己加入想要的圖層）
+  var img = new Image(); // 新增圖像1
+  img.src = "img/overlay.png"; //圖像路徑（路徑自己設，且自己加入想要的圖層）
 	var img2 = new Image(); //新增圖像2
 	img2.src = "img/overlayback.png" //圖像路徑
 	var img3 = new Image();//新增圖像3
@@ -133,7 +133,7 @@ FB.getLoginStatus(function(response) {
       // if the drag flag is set, clear the canvas and draw the image
       if(isDragging){ //當拖拉為True時
           	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-			var profileIMG = document.getElementById("preview1");//抓html裡預載入的照片
+			var profileIMG = document.getElementById("putImg");//抓html裡預載入的照片
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			//canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			//canvas.height = profileIMG.height;
