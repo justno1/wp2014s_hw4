9ad1f7905917de3b3f54d3008b82c140cb221d26
justno1 <justno1@hotmail.com.tw>
2014-06-11 00:11:28 +0800

    try

--- js/script.js
+++ js/script.js
@@ -133,7 +133,7 @@ FB.getLoginStatus(function(response) {
       // if the drag flag is set, clear the canvas and draw the image
       if(isDragging){ //當拖拉為True時
           	ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-			var profileIMG = document.getElementById("preview1").src;//抓html裡預載入的照片
+			var profileIMG = document.getElementById("preview1").value;//抓html裡預載入的照片
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			canvas.width = profileIMG.width;//設定canvas的大小需符合profileimg的大小
 			canvas.height = profileIMG.height;
