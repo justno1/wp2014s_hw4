3e1cb2544e89675b78c20b2e2c654619c27be4b2
justno1 <justno1@hotmail.com.tw>
2014-06-11 22:23:33 +0800

    ABGT add

--- js/script.js
+++ js/script.js
@@ -134,7 +134,7 @@ FB.getLoginStatus(function(response) {
       // if the drag flag is set, clear the canvas and draw the image
       if(isDragging){ //當拖拉為True時
       ctx.clearRect(0,0,canvasWidth,canvasHeight); //移除canvas起始的內容
-			var profileIMG = document.getElementById("preview1");//抓html裡預載入的照片
+			var profileIMG = document.getElementById("getPicUrl");//抓html裡預載入的照片
 			profileIMG.crossOrigin = "Anonymous"; // 這務必要做，為了讓Facebook的照片能夠crossdomain傳入到你的頁面，CORS Policy請參考https://developer.mozilla.org/en-US/docs/Web/HTML/CORS_enabled_image 
 			
 			ctx.drawImage(profileIMG,canMouseX-1/2,canMouseY-30/2);//從XY軸0，0值開始畫如profileimg
